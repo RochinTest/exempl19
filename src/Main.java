026c4ef72e1a26771e88e6f6799937a69680ec98
Rimlin <Rimlin@gmail.com>
2018-12-27 17:16:30 +0200

    exempl9

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// Выведите на экран все члены последовательности 2an-1–1, где a1=2, которые меньше 10000.
+public class Main {
+    public static void main(String[] args){
+        for(int a=2;a<10000;a=2*a-1){
+            System.out.print(a+" ");
+        }
+    }}
